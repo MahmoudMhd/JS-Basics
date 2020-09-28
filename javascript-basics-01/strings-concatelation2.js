2e6f09b3d82b2dd31de6b4e682be1e36ee7f6530
mahmoudmhd <mahmoudmehdi313@gmail.com>
2020-09-28 12:34:22 +0300

    Completed strings-concatenation2

---
+++ javascript-basics-01/strings-concatelation2.js
@@ -0,0 +1,10 @@
+ var name=prompt("Please enter your name");
+ var surname =prompt("Please enter your name");
+ var  city=prompt("Please enter your city");
+ alert(name+"\n"+surname+"\n"+city);
+function abc(){
+     name=document.getElementById("name").value;
+     surname=document.getElementById("surname").value;
+     city=document.getElementById("city").value;
+    alert(name+"\n"+surname+"\n"+city);
+}
